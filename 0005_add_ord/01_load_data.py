78895e51da73500c4710da8f79bc3d31da350ffb
wagenrace <nijhof.dns@gmail.com>
2024-06-18 10:33:58 +0200

    load it as json

--- 0005_add_ord/01_load_data.py
+++ 0005_add_ord/01_load_data.py
@@ -1,7 +1,10 @@
 # import requirements
-import gzip
+import json
+
 from ord_schema.message_helpers import load_message, write_message
 from ord_schema.proto import dataset_pb2
+from google.protobuf.json_format import MessageToJson
+
 from pathlib import Path
 
 data_path = (
@@ -11,11 +14,23 @@ data_path = (
     / "ord_dataset-00005539a1e04c809a9a78647bea649c.pb.gz"
 )
 
-with gzip.open(data_path, mode="rb") as f:
-    for line in f:
-        print(f)
-        break
-# load the binary ord file
-# dataset = load_message(str(data_path), dataset_pb2.Dataset)
-# # save the ord file as human readable text
-# write_message(dataset, "output_fname.pbtxt")
+dataset = load_message(
+    str(data_path),
+    dataset_pb2.Dataset,
+)
+
+# take one reaction message from the dataset for example
+rxn = dataset.reactions[0]
+rxn_json = json.loads(
+    MessageToJson(
+        message=rxn,
+        including_default_value_fields=False,
+        preserving_proto_field_name=True,
+        indent=2,
+        sort_keys=False,
+        use_integers_for_enums=False,
+        descriptor_pool=None,
+        float_precision=None,
+        ensure_ascii=True,
+    )
+)
---
+++ 0005_add_ord/README.md
@@ -0,0 +1,4 @@
+# Open Reaction Database
+
+You might get an error with this submodule of [open reaction database](https://github.com/open-reaction-database/ord-data).
+In that case run `git -C 0005_add_ord/ord-data lfs pull`
