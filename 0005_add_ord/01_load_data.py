c75949930ae5c081a6941692fe61f2580714d718
wagenrace <nijhof.dns@gmail.com>
2024-06-18 14:51:41 +0200

    Save reactions as JSONs

--- 0005_add_ord/01_load_data.py
+++ 0005_add_ord/01_load_data.py
@@ -1,7 +1,7 @@
 # import requirements
 import json
 
-from ord_schema.message_helpers import load_message, write_message
+from ord_schema.message_helpers import load_message
 from ord_schema.proto import dataset_pb2
 from google.protobuf.json_format import MessageToJson
 
@@ -14,23 +14,32 @@ data_path = (
     / "ord_dataset-00005539a1e04c809a9a78647bea649c.pb.gz"
 )
 
-dataset = load_message(
-    str(data_path),
-    dataset_pb2.Dataset,
-)
+data_path = Path("ord-data") / "data"
+temp_folder = Path("temp")
+temp_folder.mkdir(exist_ok=True)
 
-# take one reaction message from the dataset for example
-rxn = dataset.reactions[0]
-rxn_json = json.loads(
-    MessageToJson(
-        message=rxn,
-        including_default_value_fields=False,
-        preserving_proto_field_name=True,
-        indent=2,
-        sort_keys=False,
-        use_integers_for_enums=False,
-        descriptor_pool=None,
-        float_precision=None,
-        ensure_ascii=True,
+for gz_path in data_path.glob("*/*.pb.gz"):
+    json_name = gz_path.name.replace(".pb.gz", ".json")
+    dataset = load_message(
+        str(gz_path),
+        dataset_pb2.Dataset,
     )
-)
+
+    # take one reaction message from the dataset for example
+    for reaction in dataset.reactions:
+        rxn_json = json.loads(
+            MessageToJson(
+                message=reaction,
+                including_default_value_fields=False,
+                preserving_proto_field_name=True,
+                indent=2,
+                sort_keys=False,
+                use_integers_for_enums=False,
+                descriptor_pool=None,
+                float_precision=None,
+                ensure_ascii=True,
+            )
+        )
+
+        with open(temp_folder / json_name, "w") as fp:
+            json.dump(rxn_json, fp, indent=4)
