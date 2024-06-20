2e9ac0e45d037d8f62acb99673459168b99c4915
wagenrace <nijhof.dns@gmail.com>
2024-06-20 21:37:59 +0200

    add can SMILES too pubchem

--- 0001_populate_chemicals/04_add_can_smiles.py
+++ 0001_populate_chemicals/04_add_can_smiles.py
@@ -1,42 +1,30 @@
-# %%
 import gzip
 import json
 import os
 import re
 import urllib.request
-from math import ceil
 from time import time
 
 import pandas as pd
 from encode_for_neo4j import encode2neo4j
 
-# from py2neo import Graph
-from tqdm import tqdm
+from py2neo import Graph
 
 with open("config.json") as f:
     config = json.load(f)
 
-# port = config["port"]
-# user = config["user"]
-# pswd = config["pswd"]
-# neo4j_import_loc = config["neo4j_import_loc"]
+port = config["port"]
+user = config["user"]
+pswd = config["pswd"]
+neo4j_import_loc = config["neo4j_import_loc"]
 
-# graph = Graph("bolt://localhost:" + port, auth=(user, pswd))
-
-# # %% Create compound constrain
-# # response = graph.run(
-# #     f"""
-# #         CREATE constraint compoundId if not exists for (c:Compound) require c.pubChemCompId is unique;
-# #     """
-# # ).data()
-# print(response)
+graph = Graph("bolt://localhost:" + port, auth=(user, pswd))
 
 temp_dir = "temp"
 os.makedirs(temp_dir, exist_ok=True)
 
 
 number = 1
-synonyms_per_file = 1e6
 
 while True:
     unique_results = set()
@@ -57,57 +45,52 @@ while True:
         except:
             break
         print("download time", time() - start)
-    break
+
     start = time()
     with gzip.open(gz_file_loc, "r") as f:
         for line in f:
             line = line.decode("utf-8")
             if line.startswith("@prefix"):
                 continue
-            synonym_id, _, compound = line.split("\t")
-            compound = compound.lower()
-            result = re.findall("(.*) .\n", compound)
-            if result:
-                if len(result) > 1:
-                    print(f"Multiple hits with {compound}")
-                    continue
-                clean_synonym = encode2neo4j(result[0])
+            part1, _, part3 = line.split("\t")
+            raw_compounds = re.findall(
+                "descriptor:(.*)_canonical_smiles", part1.lower()
+            )
+            if len(raw_compounds) > 1:
+                print(f"Multiple compounds found within: {raw_compounds}")
+                continue
+            if len(raw_compounds) == 0:
+                print(f"No compounds found within: {raw_compounds}")
+                continue
+            compound = "compound:" + raw_compounds[0]
+            smiles = re.findall('"(.*)"@en .\n', part3)
 
-                result_list.append(result[0])
-            else:
-                print(f"Error with {compound}")
+            if len(smiles) > 1:
+                print(f"Multiple SMILES found within: {smiles}")
+                continue
+            if len(smiles) == 0:
+                print(f"No SMILES found within: {smiles}")
+                continue
 
-            if len(result_list) > 1000:
-                unique_results.update(set(result_list))
+            clean_smiles = encode2neo4j(smiles[0])
+
+            result_list.append({"canSMILES": smiles[0], "pubChemCompId": compound})
+
+            if len(result_list) >= 10000:
+                result_pd = pd.DataFrame(result_list)
+                result_pd.to_csv(
+                    os.path.join(neo4j_import_loc, f"canSmiles.csv"),
+                    index=False,
+                )
+                graph.run(
+                    f"""
+                    LOAD CSV  WITH HEADERS FROM 'file:///canSmiles.csv' AS row
+                    WITH row.pubChemCompId as comp_id, row.canSMILES as smiles
+                    MATCH (comp:Compound {{pubChemCompId: comp_id}})
+                    SET comp.canSMILES = smiles
+                """
+                ).data()
+                print(f"Added an other {len(result_list)}")
                 result_list = []
-    print("reading time", time() - start)
+    print("reading time of gz", time() - start)
     number += 1
-
-    print(f"total compounds: {len(unique_results)}\n")
-
-    unique_results = list(unique_results)
-    # %%
-    split_size = 1e6
-    number_files = 0
-    for i in tqdm(range(ceil(len(unique_results) / split_size))):
-        result_df = pd.DataFrame(
-            unique_results[int(i * split_size) : int((i + 1) * split_size)],
-            columns=["compound"],
-        )
-        result_df = result_df.drop_duplicates()
-        result_df.to_csv(
-            os.path.join(neo4j_import_loc, f"compounds.csv"),
-            index=False,
-        )
-        number_files += 1
-
-        graph.run(
-            f"""
-            LOAD CSV  WITH HEADERS FROM 'file:///compounds.csv' AS row
-            WITH row.compound as comp_id
-            CALL {{
-                WITH comp_id
-                MERGE (comp:Compound {{pubChemCompId: comp_id}})
-            }} IN TRANSACTIONS OF 10000 ROWS
-        """
-        ).data()
